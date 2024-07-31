bc3c16241ff5e0c7f85da54ae3b956d3689a759d
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-07-31 09:55:18 +0300

    Initial version

---
+++ src/main/java/gr/codehub/test/Main.java
@@ -0,0 +1,13 @@
+package gr.codehub.test;
+
+
+public class Main {
+
+    public static int myLuckyNumber() {
+        return 2;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World " + myLuckyNumber());
+    }
+}
---
+++ src/test/java/gr/codehub/test/MainTest.java
@@ -0,0 +1,14 @@
+package gr.codehub.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+public class MainTest {
+
+
+    @Test
+    void myLuckyNumberTest() {
+        assertEquals(1, Main.myLuckyNumber());
+    }
+}
